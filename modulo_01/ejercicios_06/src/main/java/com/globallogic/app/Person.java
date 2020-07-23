45d47d4c809fe645cf87162aae8d88b74fc7a250
roberto_suarez <roberto.suarez1994016@gmail.com>
2020-07-23 19:34:37 -0300

    ejercicios_06

---
+++ modulo_01/ejercicios_06/src/main/java/com/globallogic/app/Person.java
@@ -0,0 +1,19 @@
+package com.globallogic.app;
+
+public abstract class Person {
+	private int id;
+	private String name;
+	
+	protected int getId() {
+		return id;
+	}
+	protected void setId(int id) {
+		this.id = id;
+	}
+	protected String getName() {
+		return name;
+	}
+	protected void setName(String name) {
+		this.name = name;
+	}
+}
